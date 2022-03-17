a0211d49a0b294498c99bff9d9f97039d72c9d3b
Ninh Van <nhvan@go.olemiss.edu>
2022-03-16 21:44:16 -0500

    Cleaner Code update

--- src/App.js
+++ src/App.js
@@ -6,37 +6,49 @@ import DisplayBoard from './components/DisplayBoard';
 import { FiSearch } from 'react-icons/fi';
 function App() {
   const [city, setCity] = useState("Oxford")
-  const [zip,setZip] = useState("38655")
+  const [zip, setZip] = useState("38655")
   const [temp, setTemp] = useState()
-  const [weather,setWeather] = useState()
-  const [weatherDetails,setWeatherDetails] = useState()
-  function search(location){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=49f83cd3b65feac05c15461df3b98079&units=imperial")
-    .then((res) => res.json())
-    .then((result) => {
-      console.log(result)
-      setTemp(result.main.temp)
-      setWeather(result.weather[0].main)
-      setWeatherDetails([result.main.feels_like,result.main.humidity,result.main.pressure])
-      setCity(result.name)
+  const [weather, setWeather] = useState()
+  const [weatherDetails, setWeatherDetails] = useState()
+  async function callWeatherAPI(location) {
+    let res;
+      await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=49f83cd3b65feac05c15461df3b98079&units=imperial")
+      .then((res) => res.json())
+      .then((result) => {
+        res = result;
+      })
+    return res;
+  }
+  function setWeatherData(weatherRes) {
+    if (weatherRes) {
+      setTemp(weatherRes.main.temp)
+      setWeather(weatherRes.weather[0].main)
+      setWeatherDetails([weatherRes.main.feels_like, weatherRes.main.humidity, weatherRes.main.pressure])
+      setCity(weatherRes.name)
+    }
+  }
+  async function callGeolocationAPI() {
+    let res;
+    await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=b073c4c1e6e3415c9f160f19cd3d43d2")
+      .then((res) => res.json())
+      .then((result) => {
+          res=result;
+      })
+    return res;
+  }
+  function search(location) {
+    callWeatherAPI(location).then((weatherRes)=>{
+      setWeatherData(weatherRes)
     })
   }
   useEffect(() => {
-    // fetch("https://api.ipgeolocation.io/ipgeo?apiKey=b073c4c1e6e3415c9f160f19cd3d43d2")
-    //   .then((res) => res.json())
-    //   .then((result) => {
-    //     console.log(result)
-    //     setCity(result.city)
-    //     setZip(result.zip)
-    //   })
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + zip + "&appid=49f83cd3b65feac05c15461df3b98079&units=imperial")
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result)
-        setTemp(result.main.temp)
-        setWeather(result.weather[0].main)
-        setWeatherDetails([result.main.feels_like,result.main.humidity,result.main.pressure])
-      })
+    callWeatherAPI(zip).then((weatherRes)=>{
+      setWeatherData(weatherRes)
+    })
+    callGeolocationAPI().then((location)=>{
+      setCity(location.city)
+      setZip(location.zip)
+    })
   }, [])
   return (
     <div className="App">
--- src/components/DisplayBoard.js
+++ src/components/DisplayBoard.js
@@ -4,11 +4,17 @@ const DisplayBoard = (props) => {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
     const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const [date, setDate] = useState(new Date());
-    const [time, setTime] = useState(date.getHours() + ":" + date.getMinutes());
+    const [time, setTime] = useState();
     const [weekDay, setWeekDay] = useState(week[date.getDay()]);
     const [temp, setTemp] = useState();
     const [day, setDay] = useState(date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear());
     useEffect(() => {
+        let currTime=date.getHours() + ":"
+            if(date.getMinutes()<10)
+                currTime+="0"+date.getMinutes()
+            else
+                currTime+=date.getMinutes()
+        setTime(currTime)
     })
     return (
         <div className='display-board-container'>
